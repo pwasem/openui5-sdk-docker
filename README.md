53c0e43185f5cacb7e8324944996dc3c5a76ac8e
pwasem <pascal.wasem@googlemail.com>
2019-08-21 10:37:59 +0200

    update readme

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Browse to `http://localhost:5000/`
 Version can be defined using `ui5_version`.
 
 Enjoy!
+
+### Behind the scenes
+
+The docker image itself is build upon the latest [node:alpine](https://github.com/nodejs/docker-node) image.
+When building the docker image the specified openui5 sdk will be download from https://github.com/SAP/openui5/releases/download.
+Finally a web server using [fastify](https://www.fastify.io/) will be started to serve the openui5 sdk as fast a possible.
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const fastify = require('fastify')({
-    logger: false
-});
+const fastify = require('fastify')();
 const fastifyCors = require('fastify-cors');
 const fastifyStatic = require('fastify-static');
 
@@ -15,9 +13,7 @@ const fastifyStatic = require('fastify-static');
         });
         // listen
         await fastify.listen(3000, '0.0.0.0');
-        //fastify.log.info(`server listening on ${fastify.server.address().port}`)
     } catch (err) {
-        //fastify.log.error(err);
         process.exit(1)
     }
 })();
